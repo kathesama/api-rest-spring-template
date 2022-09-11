151d72b01a84f55bd7cc5c496bc18bcb10921560
Katherine Aguirre <katherine.aguirre@kinandcarta.com>
2022-09-11 16:51:49 -0300

    init commit

---
+++ src/main/java/com/kathesama/apirestspringtemplate/apirestspringtemplate/ApiRestSpringTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.kathesama.apirestspringtemplate.apirestspringtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestSpringTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestSpringTemplateApplication.class, args);
+	}
+
+}
